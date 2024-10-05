aad1f68c663cea916910f1d706a31b58c7e996f0
Shathananda Bhat N <shathanandabhatn@gmail.com>
2024-10-05 13:43:33 +0530

    The React Project has been updated and all config's are perfectly set

---
+++ Portfolio/src/components/navbar.jsx
@@ -0,0 +1,16 @@
+
+const navbar = () => {
+  return (
+    <div>
+        <nav className="w-full h-10 flex list-none bg-blue-400 text-white text-2xl font-serif font-semibold space-x-10">
+            <li className=""><a href="">Home</a></li>
+            <li className=""><a href="">About</a></li>
+            <li className=""><a href="">Projects</a></li>
+            <li className=""><a href="">Skills</a></li>
+            <li className=""><a href="">Contact</a></li>
+        </nav>
+    </div>
+  )
+}
+
+export default navbar
