a15018033744a61101fc1d613ee731c047fdfe17
Shathananda Bhat N <shathanandabhatn@gmail.com>
2024-10-07 15:39:02 +0530

    Updated

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,9 +1,20 @@
+import { Route, Routes } from 'react-router-dom'
 import Navbar from './components/navbar'
+import Home from './pages/Home'
+import About from './pages/About'
+import Projects from './pages/Projects'
+import Skills from './pages/Skills'
 
 function App() {
   return (
     <>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Projects" element={<Projects />} />
+        <Route path="/Skills" element={<Skills />} />
+      </Routes>
     </>
   )
 }
--- Portfolio/src/components/navbar.jsx
+++ Portfolio/src/components/navbar.jsx
@@ -1,14 +1,14 @@
-import Home from '../pages/Home'
+import { Link } from 'react-router-dom';
 
 const navbar = () => {
   return (
     <div>
         <nav className="w-full h-16 list-none font-serif font-extrabold text-2xl flex justify-end space-x-5 px-2 py-5 bg-blue-400">
-            <li className=""><a href="">Home</a></li>
-            <li className=""><a href="">About</a></li>
-            <li className=""><a href="">Projects</a></li>
-            <li className=""><a href="">Skills</a></li>
-            <li className=""><a href="">Contact</a></li>
+            <li className=""><Link to="/">Home</Link></li>
+            <li className=""><Link to="/About">About</Link></li>
+            <li className=""><Link to="Projects">Projects</Link></li>
+            <li className=""><Link to="Skills">Skills</Link></li>
+            <li className=""><Link to="Contact">Contact</Link></li>
         </nav>
     </div>
   )
